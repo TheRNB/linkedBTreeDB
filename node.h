0b1fd7c6b90ab8c37c3cf48419f3a2b4e31ffa9a
Aaron <aranbateni@gmail.com>
2022-02-04 19:11:15 +0330

    added functinality to BTNode

--- node.h
+++ node.h
@@ -1,5 +1,5 @@
 const int CHILD_MIN = 3;
-const int CHILD_MAX = 6;
+const int CHILD_MAX = 5;
 
 class Node{
 public:
@@ -11,10 +11,36 @@ public:
 class BTNode {
 public:
     BTNode() {
+        sizee = 0;
         parentIndex = -1;
+        for (int i = 0; i < CHILD_MAX; ++i)
+            keys[i] = children[i] = NULL;
+    }
+    void addNode(Node* newNode, int posID = 0) {
+        // ADDS A NODE SOLELY IN BTNode AND NOT THE CHILDREN
+        for (int i = CHILD_MAX; i > posID; --i) {
+            keys[i] = keys[i-1];
+            children[i] = children[i-1];
+        }
+        return;
+    }
+    void delNode(int posID = 0) {
+        // DELETES A NODE SOLELY IN BTNode AND NOT THE CHILDREN
+        for (int i = posID; i <= CHILD_MAX; ++i) {
+            keys[i] = keys[i+1];
+            children[i] = children[i+1];
+        }
+        return;
+    }
+    int size() {
+        return sizee;
+    }
+    int index() {
+        return parentIndex;
     }
 private:
-    Node* keys[CHILD_MAX - 1];
-    Node* children[CHILD_MAX];
+    Node* keys[CHILD_MAX + 1];
+    Node* children[CHILD_MAX + 1];
     int parentIndex; //index of the node among its parent
+    int sizee;
 };
