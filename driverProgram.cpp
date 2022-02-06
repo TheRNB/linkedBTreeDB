f636316fe0b8b4d84be053f8a8425919ddf076b7
Aaron <aranbateni@gmail.com>
2022-02-07 00:46:38 +0330

    added a driver program for the database

---
+++ driverProgram.cpp
@@ -0,0 +1,13 @@
+#include "database.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string inputString = "";
+	while (getline(cin, inputString)) {
+		//pass
+	}
+	return 0;
+}
